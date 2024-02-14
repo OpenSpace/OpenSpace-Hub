1f356f5da5282731cbc14bf5ec0a33904a56a670
Abhay Garg <ag9489@nyu.edu>
2024-02-14 17:14:17 -0500

    underline-on-active in navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -9,19 +9,19 @@ const NavBar = () => (
             <Navbar.Brand href="#home">OpenSpaceHub</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#assets">Assets</Nav.Link>
-                    <Nav.Link href="#profiles">Profiles</Nav.Link>
+                <Nav variant="underline" defaultActiveKey="/home" className="me-auto">
+                    <Nav.Link href="#assets" className="underline-on-active">Assets</Nav.Link>
+                    <Nav.Link href="#profiles" className="underline-on-active">Profiles</Nav.Link>
                     <NavDropdown title="Media" id="collapsible-nav-dropdown">
-                        <NavDropdown.Item href="#session-recordings">Session Recordings</NavDropdown.Item>
-                        <NavDropdown.Item href="#videos">Videos</NavDropdown.Item>
+                        <NavDropdown.Item href="#session-recordings" className="underline-on-active">Session Recordings</NavDropdown.Item>
+                        <NavDropdown.Item href="#videos" className="underline-on-active">Videos</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#some-other-items">Some other items</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#login">Login</Nav.Link>
-                    <Nav.Link eventKey={2} href="#Signup">Sign Up</Nav.Link>
+                    <Nav.Link href="#login" className="underline-on-active">Login</Nav.Link>
+                    <Nav.Link eventKey={2} href="#Signup" className="underline-on-active">Sign Up</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
